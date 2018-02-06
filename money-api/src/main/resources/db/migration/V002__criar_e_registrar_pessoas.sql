3197b45981b24300c13d84d8f95c221f34be0a19
André Luís <tr.andreluis@gmail.com>
2018-02-05 21:14:49 -0300

    Migration: Inserção de Pessoas sem endereço, remoção de Not Null's

--- money-api/src/main/resources/db/migration/V002__criar_e_registrar_pessoas.sql
+++ money-api/src/main/resources/db/migration/V002__criar_e_registrar_pessoas.sql
@@ -2,20 +2,19 @@ CREATE TABLE pessoa(
 	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50) NOT NULL,
 	ativo BOOLEAN DEFAULT NULL,
-	logradouro VARCHAR(50) NOT NULL,
-	numero VARCHAR(6) NOT NULL,
-	complemento VARCHAR(20) NOT NULL,
-	bairro VARCHAR(25) NOT NULL,
-	cep VARCHAR(10) NOT NULL,
-	cidade VARCHAR(30) NOT NULL,
-	estado VARCHAR(50) NOT NULL
+	logradouro VARCHAR(50),
+	numero VARCHAR(6),
+	complemento VARCHAR(20),
+	bairro VARCHAR(25),
+	cep VARCHAR(10),
+	cidade VARCHAR(30),
+	estado VARCHAR(50)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado)
 VALUES ('Severino', 1, 'Rua Nova', '10', 'Casa azul', 'Centro', '56900-000', 'Monteiro', 'Paraíba');
 
-INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado)
-VALUES ('Cleyton', 0, 'Rua Velha', '19', 'Orelhão', 'Antigo', '84903-000', 'Olinda', 'Pernambuco');
+INSERT INTO pessoa (nome, ativo) VALUES ('Cleyton', 0);
 
 INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado)
 VALUES ('André', 1, 'Av. Pres. Vargas', '10', 'Apt', 'Cabedelo', '37821-542', 'Prata', 'Paraíba');
@@ -23,8 +22,7 @@ VALUES ('André', 1, 'Av. Pres. Vargas', '10', 'Apt', 'Cabedelo', '37821-542', '
 INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado)
 VALUES ('Davi', 0, 'Rua 7', '8', 'Janelas verdes', 'Tamarineira', '325089-88', 'Serra Talhada', 'Pernambuco');
 
-INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado)
-VALUES ('Vanderlan', 1, 'Av. Treze de Julho', '43', 'Capão', 'Maldivas', '62966-093', 'Salvador', 'Bahia');
+INSERT INTO pessoa (nome, ativo) VALUES ('Vanderlan', 1);
 
 INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado)
 VALUES ('Luís', 1, 'Rua da Ferrovia', '76', 'Terceiro andar', 'Lusitana', '87098-491', 'Curituba', 'Paraná');
