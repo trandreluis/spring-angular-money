83fb4f041bb18defa6b6b336afca9d2193ed147a
André Luís <tr.andreluis@gmail.com>
2018-02-06 21:38:38 -0300

    Implementação do DELET no REST para remover recursos Pessoas

--- money-api/src/main/java/com/trandreluis/money/api/resource/PessoaResource.java
+++ money-api/src/main/java/com/trandreluis/money/api/resource/PessoaResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.trandreluis.money.api.event.RecursoCriadoEvent;
@@ -51,6 +53,12 @@ public class PessoaResource {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(pessoaEncontrada);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long id) {
+		pessoaRepository.delete(id);
 	}
 
 }
