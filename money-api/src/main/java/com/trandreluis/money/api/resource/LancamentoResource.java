f460b84afe8e6ba3b638943773895f601d12edd8
André Luís <tr.andreluis@gmail.com>
2018-02-15 20:44:18 -0300

    Implementação do cadastro com verbo POST na API REST para recursos do tipo Lancamento

--- money-api/src/main/java/com/trandreluis/money/api/resource/LancamentoResource.java
+++ money-api/src/main/java/com/trandreluis/money/api/resource/LancamentoResource.java
@@ -2,13 +2,20 @@ package com.trandreluis.money.api.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.trandreluis.money.api.event.RecursoCriadoEvent;
 import com.trandreluis.money.api.model.Lancamento;
 import com.trandreluis.money.api.repository.LancamentoRepository;
 
@@ -19,9 +26,21 @@ public class LancamentoResource {
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Lancamento> listar() {
 		return lancamentoRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getId()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
 	}
 	
 	@GetMapping("/{id}")
