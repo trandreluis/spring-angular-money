d778df284f1da741522cc0f124d836f1306e5f26
André Luís <tr.andreluis@gmail.com>
2018-03-07 03:36:09 -0300

    Implementação da paginacao de Lancamentos

--- money-api/src/main/java/com/trandreluis/money/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ money-api/src/main/java/com/trandreluis/money/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,13 +1,13 @@
 package com.trandreluis.money.api.repository.lancamento;
 
-import com.trandreluis.money.api.repository.filter.LancamentoFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.trandreluis.money.api.model.Lancamento;
-
-import java.util.List;
-
+import com.trandreluis.money.api.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
-	
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
+
 }
