11e02d800b4675629d90ff90f598e315d9d99c63
André Luís <tr.andreluis@gmail.com>
2018-03-06 21:23:13 -0300

    Implementação na API REST da exclusão de Lancamento com verbo DELETE

--- money-api/src/main/java/com/trandreluis/money/api/resource/LancamentoResource.java
+++ money-api/src/main/java/com/trandreluis/money/api/resource/LancamentoResource.java
@@ -12,12 +12,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.trandreluis.money.api.event.RecursoCriadoEvent;
@@ -62,6 +64,12 @@ public class LancamentoResource {
 	public ResponseEntity<Lancamento> buscarPeloId(@PathVariable Long id) {
 		Lancamento lancamento = lancamentoRepository.findOne(id);
 		return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long id) {
+		lancamentoRepository.delete(id);
 	}
 	
 	@ExceptionHandler( {PessoaInexistenteOuInativaException.class} )
