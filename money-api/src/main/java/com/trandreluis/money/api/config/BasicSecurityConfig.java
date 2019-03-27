34850162e6783a3ecc4b57e54dc16abc7238b943
André Luís <tr.andreluis@gmail.com>
2019-03-27 00:15:27 -0300

    Implementação da alteração entre @Profiles (produção e desenvolvimento)

--- money-api/src/main/java/com/trandreluis/money/api/config/BasicSecurityConfig.java
+++ money-api/src/main/java/com/trandreluis/money/api/config/BasicSecurityConfig.java
@@ -1,29 +1,20 @@
 package com.trandreluis.money.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
-@Profile("oauth-security")
-@Configuration
+@Profile("basic-security")
 @EnableWebSecurity
-@EnableResourceServer
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+public class BasicSecurityConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
@@ -36,12 +27,13 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/categorias").permitAll()
 			.anyRequest().authenticated()
 		.and()
-			.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().csrf().disable();
+			.httpBasic()
+		.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+			.csrf().disable();
 	}
 
 	@Override
@@ -51,11 +43,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	private PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public MethodSecurityExpressionHandler createExpressionHanlder() {
-		return new OAuth2MethodSecurityExpressionHandler();
 	}
 
 }
